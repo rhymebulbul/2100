e34a4ff027aeae718ec5c11a9a05cd9e22e1134f
Rhyme <35032706+rhymebulbul@users.noreply.github.com>
2022-08-21 15:36:48 +1000

    Implement task1

--- 31865224_fileutil.c
+++ 31865224_fileutil.c
@@ -2,3 +2,69 @@
 Rhyme Bulbul 31865224 
 19/8/22
 */
+
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+//#include<stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    char str[] = "Success!\n";
+    char buffer[1024];
+    int i, fd, t, l;
+    int whitespaces[10];
+    int j=0;
+
+    // write(1, str, strlen(str));
+    
+    fd = open("sample", O_RDONLY);
+    if ( fd < 0 ) {
+        //perror(strerror(1));
+        exit(1);
+    } else {
+
+        int r = read(fd, buffer, 1024);
+
+        for (int i = 0; i < r; ++i) {
+                if (isspace(buffer[i])){
+                    if (!isspace(buffer[i+1])){
+                        whitespaces[j] = i;
+                        //printf("Whitespace (%d) is: %d\n",j, i);
+                        j++;
+                    }
+            }
+        }
+    
+
+        lseek(fd, 0, SEEK_SET);
+        read(fd, buffer, whitespaces[0]);
+        write(1, buffer, whitespaces[0]);
+        
+        if(j>9){
+            j = 9;
+        }
+        //printf("%d\n", j);
+        for(int i=0; i<j; i++){
+            lseek(fd, whitespaces[i], SEEK_SET);
+            l = whitespaces[i+1]-whitespaces[i];
+            read(fd, buffer, l);
+            write(1, buffer, l);
+        }
+        exit(0);
+        
+    }
+
+
+    close(fd);
+    
+    return 0;
+}
+
