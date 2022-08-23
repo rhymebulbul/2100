ef9661fd9da32fff2bf0d471a10f0fe9b73b8512
Rhyme <35032706+rhymebulbul@users.noreply.github.com>
2022-08-23 22:40:35 +1000

    complete one and two

--- 31865224_fileutil.c
+++ 31865224_fileutil.c
@@ -1,8 +1,6 @@
-/* 
-Rhyme Bulbul 31865224 
-19/8/22
-*/
-
+//
+// Created by rhyme on 23/08/22.
+//
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
@@ -13,96 +11,125 @@ Rhyme Bulbul 31865224
 #include <ctype.h>
 #include<stdio.h>
 
-
-
-int main(int argc, char *argv[])
-{
-    char* append = "Append successful\n";
+int one(){
+    /*
+     *  Function to output the first 10 words of the sample file.
+     *  Shows entire file contents if the file contains less than 10 words.
+     */
+    int filedescriptor, wordlength;
+    int wordsprinted = 0;
     char buffer[1024];
-    int fd;
-    int j=0;
-    char* home;
-    int destination = 1;
-    int numwords = 10;
-
-
-
-    if (argc == 2){
-        // use given file
-        home = getenv("HOME");
-        strcat(home, argv[1]);
-        fd = open(home, O_RDONLY);
-    } else {
-        // Use sample file
-        fd = open("sample", O_RDONLY);
-    }
-
-    for (int i=1; i<argc; i++){
-//        printf("%s\n", argv[i]);
-//        printf("%c\n", argv[i][0]);
-        if ( argv[i][0] == '-' ){
-            if ( argv[i][1] == 'a' ){
-                // destination: argv[i+1]
-//                printf("Option: %s\n",argv[i]);
-//                printf("Command: %s\n",argv[i+1]);
-                home = getenv("HOME");
-                strcat(home, argv[i+1]);
-                destination = open(home, O_APPEND | O_CREAT);
-
-                write(1, append, strlen(append));
-
-            } else if ( argv[i][1] == 'n' ){
-                // numword: argv[i+1]
-//                printf("Option: %s\n",argv[i]);
-//                printf("Command: %s\n",argv[i+1]);
-                numwords = atoi(argv[i+1]);
-                printf("%d\n", numwords);
-            }
-        }
-    }
-
-    if ( fd < 0 ) {
-        perror("File does not exist!");
+    int whitespaces[10];
+    // Open file named sample in read only mode and store file descriptor
+    filedescriptor = open("sample", O_RDONLY);
+    // Check if file descriptor is valid
+    if ( filedescriptor<0 ) { // Print error if failed to open file and exit with status one
+        perror(strerror(1));
         exit(1);
-    } else {
-        int r = read(fd, buffer, 1024);
-        int whitespaces[numwords];
-        for (int i = 0; i < r; ++i) {
-                if (isspace(buffer[i])){
-                    if (!isspace(buffer[i+1])){
-                        whitespaces[j] = i;
-                        //printf("Whitespace (%d) is: %d\n",j, i);
-                        j++;
-                    }
+    } else { // Proceed if file opened succeessfully
+        // Read file length in number of chars
+        int filelength = read(filedescriptor, buffer, 1024);
+        // Iterate over each char and check for whitespaces
+        for (int i = 0; i < filelength; ++i) {
+            if (isspace(buffer[i])){ // Checks if the current char is a whitespace
+                if (!isspace(buffer[i+1])){ // Checks if the next char is a char and not a whitespace
+                    whitespaces[wordsprinted] = i; // Stores the location of the whitespace
+                    wordsprinted++; // Increments counter for stored whitespace locations
+                }
             }
         }
-
-        lseek(fd, 0, SEEK_SET);
-        read(fd, buffer, whitespaces[0]);
-        write(destination, buffer, whitespaces[0]);
-
-        //printf("%d %d\n", numwords, j);
-
-        if(j>numwords){
-            j = numwords-1;
+        // Print first word
+        lseek(filedescriptor, 0, SEEK_SET); // Move file offset to beginning of file
+        read(filedescriptor, buffer, whitespaces[0]);   // Reads in chars by char till first whitespace
+        write(1, buffer, whitespaces[0]);   // Writes out first word to console
+        // Checks if there are more than 10 words in sample file and ignores the rest
+        if (wordsprinted>9) { // Check if more than 10 words
+            wordsprinted = 9; // Ignore words after the 10th
         }
-        //printf("%d\n", j);
-        for(int i=0; i<j; i++){
-            lseek(fd, whitespaces[i], SEEK_SET);
-            int l = whitespaces[i+1]-whitespaces[i];
-            read(fd, buffer, l);
-            write(destination, buffer, l);
+        // Print rest of the words
+        for(int i=0; i<wordsprinted; i++){  // Iterate over at least first 10 words of sample file
+            lseek(filedescriptor, whitespaces[i], SEEK_SET);    // Move file offset to start of this word
+            wordlength = whitespaces[i+1] - whitespaces[i]; // Get length of word to print
+            read(filedescriptor, buffer, wordlength);   // Read word into buffer
+            write(1, buffer, wordlength);   // Print word from buffer
         }
-
-
+        close(filedescriptor);
     }
-
-    close(destination);
-
-    close(fd);
-    //exit(0);
     return 0;
 }
 
+int two(char path[]){
+    //printf("%s\n",path);
+    /*
+     *  Function to output the first 10 words of the sample file.
+     *  Shows entire file contents if the file contains less than 10 words.
+     */
+    int filedescriptor, wordlength;
+    int wordsprinted = 0;
+    char buffer[1024];
+    int whitespaces[10];
+//    if (path == NULL){
+//        // Open file named sample in read only mode and store file descriptor
+//        filedescriptor = open("sample", O_RDONLY);
+//    }else{
+        filedescriptor = open(path, O_RDONLY);
+    //}
+    // Check if file descriptor is valid
+    if ( filedescriptor<0 ) { // Print error if failed to open file and exit with status one
+        perror(strerror(1));
+        exit(1);
+    } else { // Proceed if file opened succeessfully
+        // Read file length in number of chars
+        int filelength = read(filedescriptor, buffer, 1024);
+        // Iterate over each char and check for whitespaces
+        for (int i = 0; i < filelength; ++i) {
+            if (isspace(buffer[i])){ // Checks if the current char is a whitespace
+                if (!isspace(buffer[i+1])){ // Checks if the next char is a char and not a whitespace
+                    whitespaces[wordsprinted] = i; // Stores the location of the whitespace
+                    wordsprinted++; // Increments counter for stored whitespace locations
+                }
+            }
+        }
+        // Print first word
+        lseek(filedescriptor, 0, SEEK_SET); // Move file offset to beginning of file
+        read(filedescriptor, buffer, whitespaces[0]);   // Reads in chars by char till first whitespace
+        write(1, buffer, whitespaces[0]);   // Writes out first word to console
+        // Checks if there are more than 10 words in sample file and ignores the rest
+        if (wordsprinted>9) { // Check if more than 10 words
+            wordsprinted = 9; // Ignore words after the 10th
+        }
+        // Print rest of the words
+        for(int i=0; i<wordsprinted; i++){  // Iterate over at least first 10 words of sample file
+            lseek(filedescriptor, whitespaces[i], SEEK_SET);    // Move file offset to start of this word
+            wordlength = whitespaces[i+1] - whitespaces[i]; // Get length of word to print
+            read(filedescriptor, buffer, wordlength);   // Read word into buffer
+            write(1, buffer, wordlength);   // Print word from buffer
+        }
+        close(filedescriptor);
+    }
+    return 0;
+}
 
-
+int main(int argc, char *argv[]) {
+    //char* home;
+    if (argc == 1){
+        /*
+         * Basic task one
+         * Call one
+         */
+        //one();
+        two("sample");
+    } else if (argc >= 2){
+        printf("%d\n", argc);
+        //printf("%s\n", argv[1]);
+        if (argv[1][0] != '-'){ // Check if argument is not -option
+            if (argv[1][0] == '/'){ // Check if argument is path
+                //home = getenv("HOME");
+                //strcat(home, argv[1]);
+                two(argv[1]);  // Call task two
+            }
+        }
+    }
+    //exit(0);
+    return 0;
+}
